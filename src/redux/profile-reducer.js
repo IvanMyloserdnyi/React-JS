ac4ff719272dde1bb0dfd1ed02e0f628aa1a7e9f
IvanMyloserdnyi <116162589+IvanMyloserdnyi@users.noreply.github.com>
2023-11-10 00:15:23 +0200

    profile-reducer tests

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -3,6 +3,7 @@ import {profileAPI as profileApi, usersAPI} from "../api/api";
 const ADD_POST = 'ADD-POST';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
 const SET_STATUS = 'SET_STATUS';
+const DELETE_POST = 'DELETE_POST';
 
 let initialState = {
         postsData: [
@@ -47,6 +48,8 @@ const profileReducer = (state=initialState, action) => {
             return {...state, profile: action.profile}
         case SET_STATUS:
             return {...state, status: action.status}
+        case DELETE_POST:
+            return {...state, postsData: state.postsData.filter((p) => p.id !== action.postId)}
         default:
             return state;
     }
@@ -55,6 +58,7 @@ const profileReducer = (state=initialState, action) => {
 export const addPostActionCreator = (newPostText) => ({type: ADD_POST, newPostText});
 export const setUserProfile = (profile) => ({type:SET_USER_PROFILE, profile});
 export const setStatus = (status) => ({type:SET_STATUS, status})
+export const deletePost = (postId) => ({type:DELETE_POST, postId})
 export const getProfileDataThunk = (userId) => {
     return (dispatch) => {
 /*        if (!userId) {
---
+++ src/redux/profile-reducer.test.js
@@ -0,0 +1,58 @@
+import profileReducer, {addPostActionCreator, deletePost} from "./profile-reducer";
+
+
+let state = {
+    postsData: [
+        {
+            id: 1,
+            likesCount: 0,
+            message: "Hi,i am a Killer Goose,be careful!!",
+            url: "https://pbs.twimg.com/media/EFIiRRyXYAA_xGw?format=jpg&name=900x900"
+        },
+        {
+            id: 2,
+            likesCount: 5,
+            message: "Hi,MOTHER OF GOD!!",
+            url: "https://img.freepik.com/premium-photo/a-duck-wearing-sunglasses-isolated-on-background_875864-671.jpg"
+        },
+        {
+            id: 3,
+            likesCount: 228,
+            message: "GA!GA!GA!GAGAGA!GAAAA!GA))0)0)MOTHERFUCKER!",
+            url: "https://th.bing.com/th/id/R.86848a8a6a3896bc5028ecc6efa51bf5?rik=Yx8NeD1z7BSRtg&riu=http%3a%2f%2fandrey-eltsov.ru%2fwp-content%2fuploads%2f2017%2f09%2fSmehAva20-300x300.jpg&ehk=uPm%2flcEuEcbW8qcUnpsCq2Se%2bCFDxEFJzpujUNT5ALA%3d&risl=&pid=ImgRaw&r=0"
+        }
+    ]
+}
+test('length of posts should be incremented', () => {
+    //1. test data
+    let action = addPostActionCreator('new post text')
+    //2. action
+    let newState = profileReducer(state, action)
+    //3. expectation
+    expect(newState.postsData.length).toBe(4)
+    expect(newState.postsData[3].message).toBe('new post text')
+})
+test('message of new post should be correct', () => {
+    //1. test data
+    let action = addPostActionCreator('new post text')
+    //2. action
+    let newState = profileReducer(state, action)
+    //3. expectation
+    expect(newState.postsData[3].message).toBe('new post text')
+})
+test('after deleting length of message should be decrement', () => {
+    //1. test data
+    let action = deletePost(1)
+    //2. action
+    let newState = profileReducer(state, action)
+    //3. expectation
+    expect(newState.postsData.length).toBe(2 )
+})
+test('after deleting length shouldn`t be decrement if id is incorrect', () => {
+    //1. test data
+    let action = deletePost(1000)
+    //2. action
+    let newState = profileReducer(state, action)
+    //3. expectation
+    expect(newState.postsData.length).toBe(3 )
+})
